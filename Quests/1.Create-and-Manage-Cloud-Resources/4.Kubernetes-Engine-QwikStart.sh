23bc92e3b1b658880ce70e1f7ef767e1e570140a
user9-21 <92918900+user9-21@users.noreply.github.com>
2022-12-21 22:05:28 +0530

    updated

--- Quests/1.Create-and-Manage-Cloud-Resources/4.Kubernetes-Engine-QwikStart.sh
+++ Quests/1.Create-and-Manage-Cloud-Resources/4.Kubernetes-Engine-QwikStart.sh
@@ -3,22 +3,14 @@ export PROJECT=$GOOGLE_CLOUD_PROJECT
 curl -o default.sh https://raw.githubusercontent.com/gcp-q/GCCP/main/files/default.sh
 source default.sh
 
-while [[ $VERIFY_DETAILS != 'y' ]];
-do echo " " && 
-read -p "${BOLD}${YELLOW}Enter Region name   : ${RESET}" REGION_NAME && 
-read -p "${BOLD}${YELLOW}Enter zone : ${RESET}" ZONE && 
-echo "${BOLD} " && 
-echo "${YELLOW}Region : ${CYAN}$REGION_NAME  " && 
-echo "${YELLOW}zone : ${CYAN}$ZONE  " && 
-echo " " && 
-read -p "${BOLD}${YELLOW}Verify all details are correct? [ y/n ] : ${RESET}" VERIFY_DETAILS;
-done
-
-gcloud config set compute/zone us-central1-a
-gcloud container clusters create my-cluster
-gcloud container clusters get-credentials my-cluster
+gcloud config set compute/zone us-east1-d
+gcloud container clusters create --machine-type=e2-medium --zone=us-east1-d lab-cluster 
+gcloud container clusters get-credentials lab-cluster 
 kubectl create deployment hello-server --image=gcr.io/google-samples/hello-app:1.0
 kubectl expose deployment hello-server --type=LoadBalancer --port 8080
 kubectl get service
+sleep 10
+kubectl get service
 
-gcloud container clusters delete my-cluster
+gcloud container clusters delete lab-cluster 
+
