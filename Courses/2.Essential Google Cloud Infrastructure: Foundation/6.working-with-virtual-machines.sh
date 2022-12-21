d612eb60b0b6d85330ba30c150f614617f338d11
user9-21 <92918900+user9-21@users.noreply.github.com>
2022-12-21 14:24:38 +0530

    updated

--- Courses/2.Essential Google Cloud Infrastructure: Foundation/6.working-with-virtual-machines.sh
+++ Courses/2.Essential Google Cloud Infrastructure: Foundation/6.working-with-virtual-machines.sh
@@ -11,51 +11,91 @@ gcloud compute instances create mc-server --zone=us-central1-a --machine-type=e2
 
 PROJECT_NUMBER=`gcloud projects describe $PROJECT_ID --format="value(projectNumber)"`
 
-echo "${YELLOW}${BOLD}visit ${BLUE}
-https://ssh.cloud.google.com/v2/ssh/projects/$PROJECT_ID/zones/us-central1-a/instances/mc-server?authuser=0&hl=en_US&projectNumber=$PROJECT_NUMBER&nonAdminProxySessionReason=1&troubleshoot4005Enabled=true&troubleshoot255Enabled=true ${YELLOW} and run below code
+export YOUR_BUCKET_NAME=$PROJECT_ID
 
-${MAGENTA}
-
+cat > ssh.sh <<EOF
 sudo mkdir -p /home/minecraft
 sudo mkfs.ext4 -F -E lazy_itable_init=0,\
 lazy_journal_init=0,discard \
 /dev/disk/by-id/google-minecraft-disk
 sudo mount -o discard,defaults /dev/disk/by-id/google-minecraft-disk /home/minecraft
+
+echo "${GREEN}${BOLD}
+Task 1 Completed
+${RESET}"
+
 sudo apt-get update
 sudo apt-get install -y default-jre-headless
 cd /home/minecraft
 sudo apt-get install -y wget
 sudo wget https://launcher.mojang.com/v1/objects/d0d0fe2b1dc6ab4c65554cb734270872b72dadd6/server.jar
 sudo java -Xmx1024M -Xms1024M -jar server.jar nogui
+
+
+echo "${GREEN}${BOLD}
+Task 2 Completed
+${RESET}"
 sudo ls -l
 sudo sed -i s/false/true/g eula.txt
-
 sudo apt-get install -y screen
+
 sudo screen -S mcs java -Xmx1024M -Xms1024M -jar server.jar nogui
 sudo screen -r mcs
-
-"
-
-gcloud compute firewall-rules create minecraft-rule --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:25565 --source-ranges=0.0.0.0/0 --target-tags=minecraft-server
-
-
-completed "Task 3"
-
-export YOUR_BUCKET_NAME=$PROJECT_ID
-echo "${YELLOW}${BOLD}
-Run this in ssh
-${MAGENTA}
 
 export YOUR_BUCKET_NAME=$PROJECT_ID
 gsutil mb gs://$YOUR_BUCKET_NAME-minecraft-backup
 
 cd /home/minecraft
 
-${YELLOW}
-Now run manually from lab page
- - create backup.sh file and further process
+echo "${BOLD}${YELLOW}${BG_RED}
+Manual step
+${RESET}${BOLD}${YELLOW}
+ - create backup.sh file manually and further process
 
-"
+${RESET}"
+EOF
+
+chmod +x ssh.sh
+gcloud compute scp --zone=us-central1-a --quiet ssh.sh mc-server:~
+sleep 5
+gcloud compute scp --zone=us-central1-a --quiet default.sh mc-server:~
+gcloud compute scp --zone=us-central1-a --quiet ssh.sh mc-server:~
+echo "${YELLOW}${BOLD}visit ${BLUE}https://ssh.cloud.google.com/v2/ssh/projects/$PROJECT_ID/zones/us-central1-a/instances/mc-server?authuser=0&hl=en_US&projectNumber=$PROJECT_NUMBER&nonAdminProxySessionReason=1&troubleshoot4005Enabled=true&troubleshoot255Enabled=true ${YELLOW} and run below code (inside ssh)
+
+
+${YELLOW}${BOLD}
+Run this in ssh
+${BG_RED}
+./ssh.sh
+${RESET}"
+
+gcloud compute firewall-rules create minecraft-rule --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:25565 --source-ranges=0.0.0.0/0 --target-tags=minecraft-server
+
+completed "Task 3"
+
+
+gcloud compute instances add-metadata mc-server --zone=us-central1-a \
+  --metadata startup-script-url=https://storage.googleapis.com/cloud-training/archinfra/mcserver/startup.sh
+
+gcloud compute instances add-metadata mc-server --zone=us-central1-a \
+  --metadata shutdown-script-url=https://storage.googleapis.com/cloud-training/archinfra/mcserver/shutdown.sh
+
+completed "Task 7"
+
+sleep 200
+
+echo "${YELLOW}${BOLD}visit ${BLUE}https://ssh.cloud.google.com/v2/ssh/projects/$PROJECT_ID/zones/us-central1-a/instances/mc-server?authuser=0&hl=en_US&projectNumber=$PROJECT_NUMBER&nonAdminProxySessionReason=1&troubleshoot4005Enabled=true&troubleshoot255Enabled=true ${YELLOW} and run below code (inside ssh)
+${MAGENTA}
+export YOUR_BUCKET_NAME=$PROJECT_ID
+gsutil mb gs://$YOUR_BUCKET_NAME-minecraft-backup
+cd /home/minecraft
+
+echo ${BOLD}${YELLOW}${BG_RED}
+Manual step (TAsk 6)
+${RESET}${BOLD}${YELLOW}
+ - create backup.sh file manually and further process
+
+${RESET}"
 
 completed "Lab"
 
